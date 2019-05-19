9375d2c17778e1cc7bf574699b445d46013b232a
solasalami <salamisola@gmail.com>
2019-05-19 18:03:05 -0400

    folders and files completed and update moved into the active repo

--- server.js
+++ server.js
@@ -10,8 +10,43 @@ var bodyParser = require("body-parser");
 
 // Sets up the Express App and the port to the host
 // =============================================================
+var app = express();
+var PORT = process.env.PORT || 3030;
 
-console.log("mongoose connected sucessfully");
+// Set up the Router
+var router = express.Router();
+
+//To require the routes to pass the router object
+require("./config/routes")(router);
+
+// Sets up the public folder as a static directory
+app.use(express.static(__dirname + "/public"));
+
+//Connecting the handlebars to the app
+app.engine("handlebars", experessHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//Applying bodyParser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// To allow all requests go through the Router
+app.use(router);
+
+// To either use the deployed database or the local mongoHeadLines Database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+
+//mongoose connection to the database
+mongoose.connect(db, function (err) {
+    //if error connecting, log it
+    if (err) {
+        console.log(err);
+    }
+    //if no error, log successful
+    else {
+        console.log("mongoose connected sucessfully");
     }
 });
 
